f22f6b150506b350b0200a42876480071073e80d
maxi-gartner <gartner.maxi@hotmail.com>
2024-07-11 23:16:20 -0300

    DETALLE

--- routes/index.js
+++ routes/index.js
@@ -17,8 +17,7 @@ const router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { 
-    title: 'Challenge',
-    subtitle: 'Challenge final',
+    title: 'Home Essentials'
   });
 });
 
