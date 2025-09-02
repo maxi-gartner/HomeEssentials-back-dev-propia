98531f317693c0412c2c9e74773bc5792ce9362a
maxi-gartner <gartner.maxi@hotmail.com>
2025-09-02 15:52:50 -0300

    error de read categories

--- controllers/categories/read.js
+++ controllers/categories/read.js
@@ -1,15 +1,19 @@
 import Category from "../../models/Category.js"
-let read = async(req, res, next) => { // la funcion controladora debe ser asincrona para poder esperar la respuesta de mongo 
-    try                             //utilizo try cath para intentar algo y agarrar los errores q puedan surgir 
-    {
-let all = await Category.find()       //utilizo find para buscaR todos los recursos del modelo que es category
-return res.status(200)                   //configuro la respuesta que le tengo que enviar al cliente(front)
-.json({
-        categories: all
-    })                            
-    }
-    catch(error){}
- console.log(error)
- return res.status(400).json({error: "a ocurrido un problema"})
+
+let read = async (req, res, next) => {
+  try {
+    // Busco todas las categorías
+    let all = await Category.find()
+
+    // Respondo al cliente con las categorías
+    return res.status(200).json({
+      categories: all,
+    })
+  } catch (error) {
+    // Capturo el error correctamente
+    console.log(error)
+    return res.status(400).json({ error: "Ha ocurrido un problema" })
   }
-  export default read
+}
+
+export default read
